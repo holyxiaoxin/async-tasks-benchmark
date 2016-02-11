605e26b8a23e3889ae3a2f4c0c4dd4dbd3cd778f
Jia Rong Lim <jr.loves.maths@gmail.com>
2016-02-11 22:55:54 +0800

    Refactor a lot of code
    Implementation stays the same, look at diff for more details.
    
    Add script options. Now, scripts can be run with the following opts:
    ./tasks -u https://example.com -m 40 -l 50
    
    Provide some useful warnings.

--- lib/tasks.ex
+++ lib/tasks.ex
@@ -1,64 +1,98 @@
 defmodule Tasks do
-  def main(_args) do
+  use Application
+
+  def main(args) do
+    args |> parse_args |> setup
+    |> process_requests |> print_results
+  end
+
+  defp parse_args(args) do
+    {options, _, _} = OptionParser.parse(args, [
+      switches: [url: :string, mul: :integer, loop: :integer],
+      aliases: [u: :url, m: :mul, l: :loop]
+    ])
+    parse_options(options)
+  end
+
+  defp parse_options([url: url, mul: mul, loop: loop]), do: {url, mul, loop}
+  defp parse_options([url: url, mul: mul]), do: {url, mul, 10} # loop defaults to 10
+  defp parse_options(_), do: nil
+
+  defp setup(nil), do: IO.puts "Please specify both url and multiplier\ne.g: ./tasks --url=http://example.com --mul=10"
+
+  # larger loop_num amount means larger consistency
+  #################################################
+  ##          Insert !!non-caching!! url         ##
+  #################################################
+  defp setup({url, mul, loop_num}) when loop_num > 0 and mul <= 1000 do
+    IO.puts "no. urls: #{mul}\nno. loops: #{Integer.to_string(loop_num)}\nprocessing requests..."
 
     # https://github.com/edgurgel/httpoison/issues/73
     # default is 50
     # https://github.com/benoitc/hackney/blob/d6676c6c447d933796c53c4aa4ce30cc084a5a47/src/hackney.app.src
     :ok = :hackney_pool.start_pool(:first_pool, [timeout: 15000, max_connections: 1000])
     HTTPoison.start
-
-    loop_num = 10 # larger amount means larger consistency
-
-    ####################################
-    ##   Insert !!non-caching!! urls  ##
-    ####################################
-    # e.g: urls = ["https://whatever", "https://whatever", "https://whatever"]
-    # Note: make sure that this list does not go beyond 1000 in size.
-    # If it does, increase hackney_pool, max_connections!
-    urls = [
-    ]
-
-    IO.puts "number of urls: #{Enum.count(urls)}"
-
     # There's a small overhead for the first request which might skew the timing results
-    if Enum.count(urls) > 0, do: HTTPoison.get(List.first(urls))  # we ignore this, due to dns lookup?
+    HTTPoison.get(url)  # we ignore this, due to dns lookup?
 
-    {total_micros, _} = :timer.tc fn ->
-      Enum.each(1..loop_num, fn _x ->
+    {List.duplicate(url, mul), loop_num}
+  end
+
+  defp process_requests({urls, loop_num}) do
+    {process_sync_requests(urls, loop_num), process_async_requests(urls, loop_num)}
+  end
+
+  defp process_sync_requests(urls, loop_num) do
+    {total_sync_micros, has_status_200} = :timer.tc fn ->
+      Enum.reduce(1..loop_num, false, fn(_x, acc) ->
         urls
           |> Enum.map(fn(url) -> HTTPoison.get(url) end)
-          # Uncomment below to check http status
-          # |> Enum.map(fn({status, result}) ->
-          #   if (status == :ok) do
-          #     Integer.to_string(result.status_code)
-          #   else
-          #     result.reason
-          #   end
-          # end)
-          # |> IO.inspect
+          |> Enum.map(fn({status, result}) ->
+            if (status == :ok) do
+              result.status_code
+            else
+              result.reason
+            end
+          end)
+          |> Enum.reduce(false, fn(x, acc) -> x == 200 || acc end)
+          |> Kernel.||(acc)
       end)
     end
-    each_micros = total_micros/loop_num
-    IO.puts "sync took an avg #{Float.round(each_micros/1000000, 4)} seconds"
+    check_status_code(:sync, has_status_200)
+    each_sync_micros = total_sync_micros/loop_num
+    "sync took an avg #{Float.round(each_sync_micros/1000000, 4)} seconds"
+  end
 
-    {total_micros, _} = :timer.tc fn ->
-      Enum.each(1..loop_num, fn _x ->
+  defp process_async_requests(urls, loop_num) do
+    {total_async_micros, has_status_200} = :timer.tc fn ->
+      Enum.reduce(1..loop_num, false, fn(_x, acc) ->
         urls
           |> Enum.map(fn(url) -> Task.async(fn -> HTTPoison.get(url) end) end)
           |> Enum.map(&Task.await(&1, 30000))
-          # Uncomment below to check http status
-          # |> Enum.map(fn({status, result}) ->
-          #   if (status == :ok) do
-          #     Integer.to_string(result.status_code)
-          #   else
-          #     result.reason
-          #   end
-          # end)
-          # |> IO.inspect
+          |> Enum.map(fn({status, result}) ->
+            if (status == :ok) do
+              result.status_code
+            else
+              result.reason
+            end
+          end)
+          |> Enum.reduce(false, fn(x, acc) -> x == 200 || acc end)
+          |> Kernel.||(acc)
       end)
     end
-    each_micros = total_micros/loop_num
-    IO.puts "async took an avg #{Float.round(each_micros/1000000, 4)} seconds"
-
+    check_status_code(:async ,has_status_200)
+    each_async_micros = total_async_micros/loop_num
+    "async took an avg #{Float.round(each_async_micros/1000000, 4)} seconds"
   end
+
+  defp check_status_code(type, false) do
+    IO.puts "!Warning: #{Atom.to_string(type)} request, cannot find at least 1 HTTP status 200"
+  end
+  defp check_status_code(_, true) do end
+
+  defp print_results({sync_print, async_print}) do
+    IO.puts sync_print
+    IO.puts async_print
+  end
+
 end
